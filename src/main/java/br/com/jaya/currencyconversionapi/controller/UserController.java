85659c161256efbda0f4e47c1033b7a6b908b156
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2021-05-12 19:46:45 -0300

    UserController created

---
+++ src/main/java/br/com/jaya/currencyconversionapi/controller/UserController.java
@@ -0,0 +1,27 @@
+package br.com.jaya.currencyconversionapi.controller;
+
+import br.com.jaya.currencyconversionapi.model.User;
+import br.com.jaya.currencyconversionapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/todo")
+    Flux<User> getAll() {
+        return service.all();
+    }
+
+}
