f6d32220ce09ec713ff2a7d5da6c1e530c8dd176
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2021-05-16 23:37:15 -0300

    test find all users

---
+++ src/test/java/br/com/jaya/currencyconversionapi/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package br.com.jaya.currencyconversionapi.service;
+
+import br.com.jaya.currencyconversionapi.domain.User;
+import br.com.jaya.currencyconversionapi.exception.CurrencyConversionException;
+import br.com.jaya.currencyconversionapi.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+class UserServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    void testFindAllUsers_ShouldBeSuccessful(){
+        var user1 = User.builder().id("609ecfbab66b6314c06af684").name("Paulo").build();
+        var user2 = User.builder().id("609ecfbab66b6314c06af685").name("George").build();
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+
+        Mockito.when(userRepository.findAll()).thenReturn(Flux.fromIterable(users));
+
+        StepVerifier.create(userService.all())
+                .expectSubscription()
+                .expectNextCount(2)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(userRepository, times(1)).findAll();
+
+    }
+}
