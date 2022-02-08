9d54d280918d520674cb90d1eedcc09e413d8011
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2022-02-08 20:45:00 -0300

    feat: endpoints documentation

--- src/main/java/br/com/jaya/currencyconversionapi/entrypoint/v1/conversion/CurrencyConversionController.java
+++ src/main/java/br/com/jaya/currencyconversionapi/entrypoint/v1/conversion/CurrencyConversionController.java
@@ -5,32 +5,50 @@ import br.com.jaya.currencyconversionapi.application.conversion.service.Conversi
 import br.com.jaya.currencyconversionapi.application.conversion.service.RatesApplicationService;
 import br.com.jaya.currencyconversionapi.application.conversion.dto.RatesResponseDto;
 import br.com.jaya.currencyconversionapi.application.conversion.dto.ConversionRequestDto;
+import br.com.jaya.currencyconversionapi.application.conversion.service.TransactionApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(value = "/conversion")
+@RequestMapping(value = "/currency-conversion")
 public class CurrencyConversionController {
 
     private final ConversionApplicationService currencyConversionService;
     private final RatesApplicationService ratesApplicationService;
+    private final TransactionApplicationService transactionApplicationService;
 
     @Autowired
     public CurrencyConversionController(ConversionApplicationService conversionApplicationService,
-                                        RatesApplicationService ratesRepository) {
+                                        RatesApplicationService ratesRepository,
+                                        TransactionApplicationService transactionApplicationService) {
         this.currencyConversionService = conversionApplicationService;
         this.ratesApplicationService = ratesRepository;
+        this.transactionApplicationService = transactionApplicationService;
     }
 
     @GetMapping(value = "/rates")
+    @Operation(summary = "Fetch currency conversion rates")
+    @ApiResponse(responseCode = "200", description = "Currency conversion rates list")
     public Mono<RatesResponseDto> getRates() {
         return ratesApplicationService.fetchRates();
     }
 
-    @PostMapping
+    @PostMapping(value = "convert")
+    @Operation(summary = "Executes conversion and then saves related transaction")
+    @ApiResponse(responseCode = "200", description = "Currency conversion successful")
     public Mono<TransactionDto> convert(@RequestBody ConversionRequestDto conversionRequestDto) {
         return currencyConversionService.convert(conversionRequestDto);
+    }
+
+    @GetMapping(value = "/user/{userId}/transactions")
+    @Operation(summary = "Fetch transactions by user id")
+    @ApiResponse(responseCode = "200", description = "Transactions retrieved successfully")
+    public Flux<TransactionDto> getTransactions(@PathVariable String userId) {
+        return transactionApplicationService.fetchTransactions(userId);
     }
 
 }
--- src/main/java/br/com/jaya/currencyconversionapi/entrypoint/v1/user/UserController.java
+++ src/main/java/br/com/jaya/currencyconversionapi/entrypoint/v1/user/UserController.java
@@ -2,6 +2,8 @@ package br.com.jaya.currencyconversionapi.entrypoint.v1.user;
 
 import br.com.jaya.currencyconversionapi.application.user.dto.UserDto;
 import br.com.jaya.currencyconversionapi.application.user.service.UserApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,8 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Fetch all users")
+    @ApiResponse(responseCode = "200", description = "Users retrieved successfully")
     public Flux<UserDto> getAll() {
         return userApplicationService.fetchUsers();
     }
