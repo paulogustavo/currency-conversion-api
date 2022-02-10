a023120f0b30c9a723a668e2651e1fc3f073d369
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2022-02-10 17:38:19 -0300

    feat: unit tests added

--- src/test/java/br/com/jaya/currencyconversionapi/entrypoint/v1/conversion/CurrencyConversionEntrypointTest.java
+++ src/test/java/br/com/jaya/currencyconversionapi/entrypoint/v1/conversion/CurrencyConversionEntrypointTest.java
@@ -2,6 +2,7 @@ package br.com.jaya.currencyconversionapi.entrypoint.v1.conversion;
 
 import br.com.jaya.currencyconversionapi.application.conversion.dto.ConversionRequestDto;
 import br.com.jaya.currencyconversionapi.application.conversion.dto.CurrencyDto;
+import br.com.jaya.currencyconversionapi.application.conversion.dto.RatesResponseDto;
 import br.com.jaya.currencyconversionapi.application.conversion.dto.TransactionResponseDto;
 import br.com.jaya.currencyconversionapi.application.conversion.service.ConversionApplicationService;
 import br.com.jaya.currencyconversionapi.application.conversion.service.RatesApplicationService;
@@ -14,9 +15,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import javax.validation.ConstraintViolationException;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
@@ -63,6 +68,65 @@ class CurrencyConversionEntrypointTest {
 
     }
 
+    @Test
+    void convert_BadRequest() {
+        //Assemble
+        var conversionRequestDto = ConversionRequestDto.builder()
+                .originCurrency(CurrencyDto.BRL)
+                .finalCurrency(CurrencyDto.AUD)
+                .value(BigDecimal.TEN)
+                //.userId("loremipsum123")
+                .build();
+
+        when(conversionApplicationService.convert(any(ConversionRequestDto.class)))
+                .thenThrow(ConstraintViolationException.class);
+
+        //Act and Assert
+        webClient.post()
+                .uri("/currency-conversion/convert")
+                .bodyValue(conversionRequestDto)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    void fetchRates_Ok() {
+        //Assemble
+        var ratesResponseDto = getRatesResponseDto();
+
+        when(ratesApplicationService.fetchRates()).thenReturn(Mono.just(ratesResponseDto));
+
+        //Act and Assert
+        webClient.get()
+                .uri("/currency-conversion/rates")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody();
+
+    }
+
+    @Test
+    void fetchTransactions_Ok() {
+        //Assemble
+        var userId = "loremipsum123";
+
+        var transactionCreateAt = new Date();
+        var transactionResponseDto = getExpectedTransactionDto(transactionCreateAt);
+
+        when(transactionApplicationService.fetchTransactions(any(String.class))).thenReturn(Flux.just(transactionResponseDto));
+
+        //Act and Assert
+        webClient.get()
+                .uri("/currency-conversion/user/{userId}/transactions", userId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody();
+
+    }
+
     private TransactionResponseDto getExpectedTransactionDto(Date createdAt){
         return TransactionResponseDto.builder()
                 .conversionRate(new BigDecimal("0.265237269075"))
@@ -74,6 +138,23 @@ class CurrencyConversionEntrypointTest {
                 .finalValue(new BigDecimal("2.65237269075"))
                 .userId("loremipsum123")
                 .build();
+    }
+
+    private RatesResponseDto getRatesResponseDto(){
+        Map<String, BigDecimal> rates = new HashMap<>();
+        rates.put("AUD", new BigDecimal("1.593275"));
+        rates.put("MXN", new BigDecimal("23.492258"));
+        rates.put("JPY", new BigDecimal("132.840936"));
+        rates.put("PLN", new BigDecimal("4.50034"));
+        rates.put("EUR", new BigDecimal("1"));
+        rates.put("USD", new BigDecimal("1.146653"));
+        rates.put("CAD", new BigDecimal("1.454816"));
+        rates.put("BRL", new BigDecimal("6.00698"));
+
+        var ratesResponse = new RatesResponseDto();
+        ratesResponse.setRates(rates);
+
+        return ratesResponse;
     }
 
 }
---
+++ src/test/java/br/com/jaya/currencyconversionapi/entrypoint/v1/user/UserEntrypointTest.java
@@ -0,0 +1,59 @@
+package br.com.jaya.currencyconversionapi.entrypoint.v1.user;
+
+import br.com.jaya.currencyconversionapi.application.user.dto.UserResponseDto;
+import br.com.jaya.currencyconversionapi.application.user.service.UserApplicationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = UserEntrypoint.class)
+@ActiveProfiles("test")
+class UserEntrypointTest {
+
+    @MockBean
+    UserApplicationService userApplicationService;
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    void fetchUsers_Ok() {
+        //Assemble
+        var userResponseDto1 = UserResponseDto.builder()
+                .id("lorem123")
+                .name("Paulo")
+                .build();
+
+        var userResponseDto2 = UserResponseDto.builder()
+                .id("ipsum123")
+                .name("Gustavo")
+                .build();
+
+        List<UserResponseDto> users = new ArrayList<>();
+        users.add(userResponseDto1);
+        users.add(userResponseDto2);
+
+        when(userApplicationService.fetchUsers()).thenReturn(Flux.fromIterable(users));
+
+        //Act and Assert
+        webClient.get()
+                .uri("/users")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody();
+
+    }
+
+}
